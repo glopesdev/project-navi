0ac4f31a679833755a7a751a04f1e2447f16b260
glopes <goncaloclopes@gmail.com>
2012-04-08 23:32:30 +0100

    Fixed image frame and skeleton frame dispose behavior in KinectFrame.

--- ProjectNavi.Bonsai.Kinect/KinectSource.cs
+++ ProjectNavi.Bonsai.Kinect/KinectSource.cs
@@ -36,24 +36,27 @@ namespace ProjectNavi.Bonsai.Kinect
 
         void kinect_AllFramesReady(object sender, AllFramesReadyEventArgs e)
         {
-            var colorFrame = e.OpenColorImageFrame();
-            var skeletonFrame = e.OpenSkeletonFrame();
-
-            if (colorFrame != null && skeletonFrame != null)
+            using(var colorFrame = e.OpenColorImageFrame())
+            using (var skeletonFrame = e.OpenSkeletonFrame())
             {
-                var bufferLength = colorFrame.Width * colorFrame.Height * colorFrame.BytesPerPixel;
-                if (colorImageBuffer == null || colorImageBuffer.Length != bufferLength)
+                if (colorFrame != null && skeletonFrame != null)
                 {
-                    colorImageBuffer = new byte[bufferLength];
-                }
+                    var bufferLength = colorFrame.Width * colorFrame.Height * colorFrame.BytesPerPixel;
+                    if (colorImageBuffer == null || colorImageBuffer.Length != bufferLength)
+                    {
+                        colorImageBuffer = new byte[bufferLength];
+                    }
 
-                colorFrame.CopyPixelDataTo(colorImageBuffer);
-                var bufferHandle = GCHandle.Alloc(colorImageBuffer, GCHandleType.Pinned);
-                var image = new IplImage(new CvSize(colorFrame.Width, colorFrame.Height), colorFrame.BytesPerPixel * 8 / 4, 4, bufferHandle.AddrOfPinnedObject());
-                var output = new IplImage(image.Size, image.Depth, 3);
-                ImgProc.cvCvtColor(image, output, ColorConversion.BGRA2BGR);
-                bufferHandle.Free();
-                Subject.OnNext(new KinectFrame(kinect, output, skeletonFrame));
+                    var skeletonData = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletonData);
+                    colorFrame.CopyPixelDataTo(colorImageBuffer);
+                    var bufferHandle = GCHandle.Alloc(colorImageBuffer, GCHandleType.Pinned);
+                    var image = new IplImage(new CvSize(colorFrame.Width, colorFrame.Height), colorFrame.BytesPerPixel * 8 / 4, 4, bufferHandle.AddrOfPinnedObject());
+                    var output = new IplImage(image.Size, image.Depth, 3);
+                    ImgProc.cvCvtColor(image, output, ColorConversion.BGRA2BGR);
+                    bufferHandle.Free();
+                    Subject.OnNext(new KinectFrame(kinect, output, skeletonData));
+                }
             }
         }
 
