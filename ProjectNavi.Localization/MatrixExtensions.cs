c6b2b4c745bd347900d8e4339da2be84db96a7bf
glopes <glopesdev@users.noreply.github.com>
2012-04-08 22:00:54 +0100

    Added draft implementation of GraphSlam for localization and mapping of markers.

---
+++ ProjectNavi.Localization/MatrixExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra.Generic;
+
+namespace ProjectNavi.Localization
+{
+    public static class MatrixExtensions
+    {
+        public static Matrix<T> Expand<T>(this Matrix<T> matrix, int rows, int columns, int[] indices) where T : struct, IEquatable<T>, IFormattable
+        {
+            return Expand(matrix, rows, columns, indices, indices);
+        }
+
+        public static Matrix<T> Expand<T>(this Matrix<T> matrix, int rows, int columns, int[] indicesI, int[] indicesJ) where T : struct, IEquatable<T>, IFormattable
+        {
+            var result = matrix.CreateMatrix(rows, columns);
+            for (int i = 0; i < indicesI.Length; i++)
+            {
+                for (int j = 0; j < indicesJ.Length; j++)
+                {
+                    result[indicesI[i], indicesJ[j]] = matrix[i, j];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix<T> Take<T>(this Matrix<T> matrix, int[] indices) where T : struct, IEquatable<T>, IFormattable
+        {
+            return Take(matrix, indices, indices);
+        }
+
+        public static Matrix<T> Take<T>(this Matrix<T> matrix, int[] indicesI, int[] indicesJ) where T : struct, IEquatable<T>, IFormattable
+        {
+            var result = matrix.CreateMatrix(indicesI.Length, indicesJ.Length);
+            for (int i = 0; i < indicesI.Length; i++)
+            {
+                for (int j = 0; j < indicesJ.Length; j++)
+                {
+                    result[i, j] = matrix[indicesI[i], indicesJ[j]];
+                }
+            }
+
+            return result;
+        }
+    }
+}
