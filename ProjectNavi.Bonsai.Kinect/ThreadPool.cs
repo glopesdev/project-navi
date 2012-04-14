878e2d6ae466ad9b27e4808e1e1ff7dc80fc55e4
glopes <glopesdev@users.noreply.github.com>
2012-04-14 21:19:21 +0100

    Added ThreadPool combinator to enable parallel processing of Bonsai streams. Minor performance adjustments. Changed render frame rate to 30 fps.

---
+++ ProjectNavi.Bonsai.Kinect/ThreadPool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bonsai.Expressions;
+using System.Reactive.Linq;
+using System.Reactive.Concurrency;
+
+namespace ProjectNavi.Bonsai.Kinect
+{
+    public class ThreadPool : CombinatorBuilder
+    {
+        protected override IObservable<TSource> Combine<TSource>(IObservable<TSource> source)
+        {
+            return source.ObserveOn(Scheduler.ThreadPool);
+        }
+    }
+}
