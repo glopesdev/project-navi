b7023e69e9cbcc39e1e2e0feae77d1403638fa9f
glopes <goncaloclopes@gmail.com>
2012-04-14 20:36:21 +0100

    Adapted KinectSource DepthFrame to output a managed array.

--- ProjectNavi.Bonsai.Kinect/KinectFrame.cs
+++ ProjectNavi.Bonsai.Kinect/KinectFrame.cs
@@ -9,14 +9,17 @@ namespace ProjectNavi.Bonsai.Kinect
 {
     public class KinectFrame
     {
-        public KinectFrame(KinectSensor sensor, IplImage colorImage, Skeleton[] skeletonData)
+        public KinectFrame(KinectSensor sensor, short[] depthImage, IplImage colorImage, Skeleton[] skeletonData)
         {
             Sensor = sensor;
+            DepthImage = depthImage;
             ColorImage = colorImage;
             SkeletonData = skeletonData;
         }
 
         public KinectSensor Sensor { get; private set; }
+
+        public short[] DepthImage { get; private set; }
 
         public IplImage ColorImage { get; private set; }
 
