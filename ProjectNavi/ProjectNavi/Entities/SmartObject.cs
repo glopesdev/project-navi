0d34b95cce2a0ecbfeaa96dc7f650b33734837d4
glopes <glopesdev@users.noreply.github.com>
2012-04-14 20:50:41 +0100

    Added KinectVisualizer and SonarVisualizer to ProjectNavi. Added first draft of steering and locomotion behaviors. Minor refactoring of SlamController. Added PrimitiveBatchRenderer to provide drawing code for some visualizers. Added first draft of SmartObject interface.

---
+++ ProjectNavi/ProjectNavi/Entities/SmartObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace ProjectNavi.Entities
+{
+    public class SmartObject
+    {
+        Collection<TaskProvider> tasks = new Collection<TaskProvider>();
+
+        public string Name { get; set; }
+
+        public Collection<TaskProvider> Tasks
+        {
+            get { return tasks; }
+        }
+    }
+}
