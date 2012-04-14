c7f24fb29d36a1095a68e6ca4fbae269e2e740ee
glopes <goncaloclopes@gmail.com>
2012-04-14 20:50:41 +0100

    Added KinectVisualizer and SonarVisualizer to ProjectNavi. Added first draft of steering and locomotion behaviors. Minor refactoring of SlamController. Added PrimitiveBatchRenderer to provide drawing code for some visualizers. Added first draft of SmartObject interface.

---
+++ ProjectNavi/ProjectNavi/Navigation/Locomotion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using ProjectNavi.Hardware;
+
+namespace ProjectNavi.Navigation
+{
+    public static class Locomotion
+    {
+        public static IEnumerable<Action<GameTime>> DifferentialSteering(Vehicle vehicle, DifferentialSteeringBoard actuators, double wheelDistance, double gain)
+        {
+            var halfDistance = wheelDistance / 2;
+            while (true)
+            {
+                yield return gameTime =>
+                {
+                    var left = vehicle.Steering.X - halfDistance * vehicle.Steering.Y;
+                    var right = vehicle.Steering.X + halfDistance * vehicle.Steering.Y;
+                    actuators.UpdateWheelVelocity(new WheelVelocity(gain * left, gain * right));
+                    vehicle.Steering = Vector2.Zero;
+                };
+            }
+        }
+    }
+}
---
+++ ProjectNavi/ProjectNavi/Navigation/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cyberiad;
+using Microsoft.Xna.Framework;
+
+namespace ProjectNavi.Navigation
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            Transform = new Transform2D();
+        }
+
+        public Transform2D Transform { get; private set; }
+
+        public Vector2 Velocity { get; set; }
+
+        public Vector2 Steering { get; set; }
+    }
+}
