64171c42383de9ba0a571f60a13dd9fd32000345
glopes <goncaloclopes@gmail.com>
2012-04-16 03:36:02 +0100

    Added smart object infrastructure and first trial implementation for sounds.

---
+++ ProjectNavi/ProjectNavi/Tasks/ActionPlayer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.Reactive.Subjects;
+using System.Reactive;
+using ProjectNavi.Navigation;
+using ProjectNavi.Hardware;
+
+namespace ProjectNavi.Tasks
+{
+    public class ActionPlayer
+    {
+        Game game;
+        IServiceProvider provider;
+        Subject<Unit> actionCompleted = new Subject<Unit>();
+        IEnumerator<Action<GameTime>> actionTask;
+
+        public ActionPlayer(Game game, IServiceProvider provider)
+        {
+            this.game = game;
+            this.provider = provider;
+        }
+
+        public IObservable<Unit> ActionCompleted
+        {
+            get { return actionCompleted; }
+        }
+
+        public void PlayAction(TaskProvider task)
+        {
+            if (actionTask != null)
+            {
+                actionTask.Dispose();
+            }
+
+            var action = task.Action(game, provider);
+            actionTask = action.GetEnumerator();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (actionTask != null)
+            {
+                if (actionTask.MoveNext()) actionTask.Current(gameTime);
+                else
+                {
+                    actionCompleted.OnNext(Unit.Default);
+                    actionTask = null;
+                }
+            }
+        }
+    }
+}
---
+++ ProjectNavi/ProjectNavi/Tasks/PlaySound.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using ProjectNavi.Navigation;
+using ProjectNavi.Hardware;
+using Microsoft.Xna.Framework.Audio;
+
+namespace ProjectNavi.Tasks
+{
+    public class PlaySound : TaskProvider
+    {
+        public string SoundName { get; set; }
+
+        public override IEnumerable<Action<GameTime>> Action(Game game, IServiceProvider provider)
+        {
+            var sound = game.Content.Load<SoundEffect>(SoundName);
+            using (var instance = sound.CreateInstance())
+            {
+                instance.Play();
+                while (instance.State == SoundState.Playing)
+                {
+                    yield return gameTime => { };
+                }
+            }
+        }
+    }
+}
